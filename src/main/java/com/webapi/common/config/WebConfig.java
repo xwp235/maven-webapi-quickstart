c1ea757cb3902ffa4a1771695bb2190ddecc57f1
xiao.wenpeng <ja_xwp@yeah.net>
2024-05-21 13:56:27 +0900

    chore: 添加后端跨域配置

--- src/main/java/com/webapi/common/config/WebConfig.java
+++ src/main/java/com/webapi/common/config/WebConfig.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.webapi.common.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,6 +25,22 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 所有api接口都应用此配置
+        registry.addMapping("/**")
+                // 允许所有来源
+                .allowedOriginPatterns(CorsConfiguration.ALL)
+                // 允许所有header
+                .allowedHeaders(CorsConfiguration.ALL)
+                // 允许所有请求方式(GET,POST,...)
+                .allowedMethods(CorsConfiguration.ALL)
+                // 允许请求带上cookie
+                .allowCredentials(true)
+                // 一小时内不再需要预检（发送OPTIONS请求）
+                .maxAge(3600);
     }
 
 }
